27a827eedde80af2b0fd16c7c193c65ff5c99c6f
Dan Kelley <kelley.dan@gmail.com>
2014-06-01 13:47:18 -0300

    fix lonlat in Rimouski landsat pic

--- sandbox/dk/rimouski.R
+++ sandbox/dk/rimouski.R
@@ -1,13 +1,18 @@
+rebuild <- TRUE
 library(oce)
-## band <- "pan"
-## image <- read.landsat("LC80120262013282LGN00", band=band, debug=0)
-## left <- -68.61465 - 0.03
-## right <- -68.42006 + 0.08
-## bottom <- 48.38358 + 0.03
-## top <- 48.64969 - 0.02
-## rimouski <- landsatTrim(image, list(latitude=bottom, longitude=left),
-##                         list(latitude=top, longitude=right))
+if (rebuild) {
+    image <- read.landsat("~/Google Drive/LC80120262013282LGN00", band="panchromatic")
+    left <- -68.61465 - 0.03
+    right <- -68.42006 + 0.08
+    bottom <- 48.38358 + 0.03
+    top <- 48.64969 - 0.02
+    rimouski <- landsatTrim(image, list(latitude=bottom, longitude=left),
+                            list(latitude=top, longitude=right))
+    save(rimouski, file="rimouski.rda")
+} else {
+    load("rimouski.rda")
+}
 if (!interactive()) png("rimouski_2013-10-09.png", width=7, height=7, unit="in", res=100, pointsize=12)
-load("rimouski.rda")
 plot(rimouski, col=oceColorsJet, zlim=c(0.125, 0.14))
+points(-68.517016, 48.444152, pch=20, col='pink')
 if (!interactive()) dev.off()
